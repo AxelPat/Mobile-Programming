0bdd84fba539b694fe84a0329cce2bab3efc8930
Axel <patriaaxel@gmail.com>
2021-02-10 16:21:59 +0700

    Week 1 of Mobile

---
+++ Week01_32597/app/src/main/java/com/example/week01_32597/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.week01_32597;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
